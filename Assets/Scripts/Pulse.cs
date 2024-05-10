57db87216a66c0b18b11e79f853aa674ec6a1d46
MWardill <mwardill@pdisoftware.com>
2024-05-10 18:05:41 +0100

    Its a pulsing egg

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    public float minScale = 0.9f;
+    public float maxScale = 1.1f;
+    public float pulseSpeed = 2f;
+
+    private float currentTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime * pulseSpeed;
+
+        float scale = Mathf.Lerp(minScale, maxScale, (Mathf.Sin(currentTime) + 1) / 2);
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
